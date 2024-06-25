693449aeed39675139c6b375a7e81e35dacd59a3
kcoolsae <kris.coolsaet@ugent.be>
2024-06-25 16:22:24 +0200

    Now commit all-tasks.pdf

--- create-all-tasks.sh
+++ create-all-tasks.sh
@@ -5,12 +5,23 @@
 #
 for dir in tasks/*/
 do
-  if [ ! -f $dir/all-tasks.pdf ]
-  then
+  if [ ! -z "$()ls -A "$dir")"]; then
+    echo "Skipping empty folder $dir"
+  elif [ -f $dir/all-tasks.pdf ]; then
+    echo "$dir/all-tasks.pdf already exists"
+  else
     echo "Creating $dir/all-tasks.pdf"
     cd $dir
     echo "Processing $dir"
     touch all-tasks.pdf
+    git add all-tasks.pdf
     cd ..
   fi
 done
+
+# commit changes if necessary
+if [ ! -z "$(git status --porcelain .)" ]; then
+  git commit -m "Add all-tasks.pdf files"
+  git push
+fi
+
